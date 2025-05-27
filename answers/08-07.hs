ac54e6c6c4bddad488121dbb3321c767532bdef2
Yusuke <yusuke@kudouyuusukenoMacBook-Air.local>
2025-05-28 08:29:41 +0900

    Change type names

--- answers/08-07.hs
+++ answers/08-07.hs
@@ -1,22 +1,22 @@
-data Maybe a = Nothing | Just a
+data MyMaybe a = MyNothing | MyJust a
 
-instance Eq a => Eq (Main.Maybe a) where
-  Main.Nothing  == Main.Nothing  = True
-  (Main.Just x) == (Main.Just y) = x == y
+instance Eq a => Eq (MyMaybe a) where
+  MyNothing  == MyNothing  = True
+  (MyJust x) == (MyJust y) = x == y
   _        == _        = False
 
--- Main.Nothing == Main.Nothing
+-- > MyNothing == MyNothing
 -- True
 
--- (Main.Just 1) == (Main.Just 1)
+-- > (MyJust 1) == (MyJust 1)
 -- True
 
--- Main.Nothing == (Main.Just 1)
+-- > MyNothing == (MyJust 1)
 -- False
 
--- (Main.Just 1) == (Main.Just 2)
+-- > (MyJust 1) == (MyJust 2)
 -- False
 
--- Duplicate declarations error
-instance Eq a => Eq [a] where
-  xs == ys = and [x == y | x <- xs, y <- ys]
+-- *Can not run in GHCi. Defined in 'GHC.Classes' already 
+--instance Eq a => Eq [a] where
+--   xs == ys = and [x == y | x <- xs, y <- ys]
